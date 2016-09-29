c968d629ec5bc7ead82748e3b1b1e1a7437398c1
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2016-09-29 02:51:19 -0400

    implemented 2.1

--- code/P2/problem2.py
+++ code/P2/problem2.py
@@ -1,3 +1,5 @@
+import math
+import numpy as np
 import matplotlib.pyplot as plt
 import pylab as pl
 
@@ -7,27 +9,43 @@ def plot_points(X, Y):
     plt.ylabel('y')
 
 def regress(X, Y, M):
-    return [-1, 1]
+    mat = []
+    for x in X:
+        row = []
+        for i in range(M+1):
+            row.append(x ** i)
+        mat.append(row)
+    Z = np.array(mat)
+    # w = (X' X)^-1 X' y
+    return np.dot(np.dot(np.linalg.inv(np.dot(np.transpose(Z), Z)), np.transpose(Z)), Y);
 
-def run():
+def curve_plot(fn, lower_x, upper_x, width=2):
+    x = [i/100.0 for i in range(100 * int(math.floor(lower_x)), 100 * int(math.ceil(upper_x)))]
+    y = []
+    for t in x:
+        y.append(fn(t))
+    plt.plot(x, y, lw=width)
+
+def run(M):
     # load data
-    data = pl.loadtxt('curvefittingp2.txt')
-    X = data[0,:]
-    Y = data[1,:]
+    data = pl.loadtxt('P2/curvefittingp2.txt')
+    X = np.array(data[0,:])
+    Y = np.array(data[1,:])
 
     # plot points
     plot_points(X, Y)
 
-    weights = regress(X, Y, 2)
-    regressed_Y = []
-    for x in X:
-        y = 0
-        for i in range(len(weights)):
-            y += weights[i] * (x ** i)
-        regressed_Y.append(y)
+    # compute and plot polynomial
+    weights = regress(X, Y, M)
+    def poly(x):
+        return sum([weights[i] * (x ** i) for i in range(len(weights))])
+    curve_plot(poly, min(X), max(X), 3)
 
-    plt.plot(X, regressed_Y, lw=2)
+    def generator(x):
+        return math.cos(math.pi * x) + math.cos(2 * math.pi * x)
+    curve_plot(generator, min(X), max(X), 1)
 
+    plt.xlabel('x')
+    plt.xlabel('y')
+    plt.title('Linear Regression (M=' + str(M) + ')' )
     plt.show()
-
-run()
